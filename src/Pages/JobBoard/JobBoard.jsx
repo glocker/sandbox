c470d356ccb00773159c95acce5fcf7e7c71845d
glocker <georgeglocker@gmail.com>
2024-10-22 00:02:12 +0200

    Simplifyed fetching data

--- src/Pages/JobBoard/JobBoard.jsx
+++ src/Pages/JobBoard/JobBoard.jsx
@@ -1,44 +1,36 @@
 import { useEffect, useState } from 'react';
 
-const JOBS_IDS_URL = 'https://hacker-news.firebaseio.com/v0/jobstories.json';
-
 export default function JobBoard() {
 
     const [isLoading, setLoading] = useState(true);
-    let [jobs, setJobs] = useState([]);
-
-    function getJobsId(jobsIdsUrl) {
-        return fetch(jobsIdsUrl)
-            .then(response => response.json());
-    }
-
-    function getJobsData(jobsToShow) {
-
-        // Request job data for every job id
-        return Promise.all(
-            jobsToShow.map((jobId) => {
-                return fetch(`https://hacker-news.firebaseio.com/v0/item/${jobId}.json`)
-                    .then(response => response.json());
-            })
-        )
-    }
+    const [jobs, setJobs] = useState([]);
 
     useEffect(() => {
-        getJobsId(JOBS_IDS_URL)
-            .then((jobsIdList) => {
+
+        // Get ids of current vacancies
+        fetch('https://hacker-news.firebaseio.com/v0/jobstories.json')
+            .then(response => response.json())
+            .then(jobsIdList => {
+
+                let jobsToShow;
 
                 // Check if Ids exists
                 if (jobsIdList && jobsIdList.length) {
 
                     // Get only first 6 jobs of all existed jobs
-                    return jobsIdList.slice(0, 6);
+                    jobsToShow = jobsIdList.slice(0, 6);
                 }
+
+                // Request job data for every job id
+                return Promise.all(
+                    jobsToShow.map((jobId) => {
+                        return fetch(`https://hacker-news.firebaseio.com/v0/item/${jobId}.json`)
+                            .then(response => response.json());
+                    }))
             })
-            .then((jobsToShow) => getJobsData(jobsToShow))
             .then(result => {
                 setJobs(result);
-                console.log(result);
-            });
+            })
     }, []);
 
     return (
