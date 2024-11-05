9c710fb1c8a001c533bbfef710eacf4fb30d3d2a
glocker <georgeglocker@gmail.com>
2024-11-05 23:03:45 +0100

    ForEach

--- src/Pages/CustomFunctions/CustomFunctions.jsx
+++ src/Pages/CustomFunctions/CustomFunctions.jsx
@@ -15,7 +15,27 @@ export default function CustomFunction() {
         return newArray;
     }
 
-    const res = arrOfNums.myMap(item => item + 1);
+    // Custom Array.prototype.map
+    Array.prototype.myForEach = function(callback) {
 
-    return (res)
+        const newArray = [];
+
+        for (let i = 0; i < this.length; i++) {
+            newArray.push(callback(this[i]));
+        }
+
+        return newArray;
+    }
+
+    const resultMap = arrOfNums.myMap(item => item + 1);
+    const resultForEach = arrOfNums.myForEach(item => item * 3);
+
+    return (
+        <>
+            <h3>Array.prototype.map</h3>
+            <div>{resultMap}</div>
+            <h3>Array.prototype.forEach</h3>
+            <div>{resultForEach}</div>
+        </>
+    )
 }
